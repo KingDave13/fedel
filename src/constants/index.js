734d1ddf4d76772e20f40e0aacd2e63c3be3cf8e
David Okaliwe <okaliwedavid@gmail.com>
2024-05-02 22:20:15 +0100

    Updates

---
+++ src/constants/index.js
@@ -0,0 +1,150 @@
+export const navLinks = [
+    {
+        id: "tiles",
+        title: "Tiles",
+        links: [
+            {
+                name: "Ceramic Tiles",
+                route: "",
+            },
+            {
+                name: "Porcelain Tiles",
+                route: "",
+            },
+            {
+                name: "Wall Tiles",
+                route: "",
+            },
+            {
+                name: "Floor Tiles",
+                route: "",
+            },
+            {
+                name: "Backsplash Tiles",
+                route: "",
+            },
+            {
+                name: "Bathroom Tiles",
+                route: "",
+            },
+            {
+                name: "Kitchen Tiles",
+                route: "",
+            },
+            {
+                name: "Indoor Tiles",
+                route: "",
+            },
+            {
+                name: "Outdoor Tiles",
+                route: "",
+            },
+            {
+                name: "Pool Tiles",
+                route: "",
+            },
+            {
+                name: "Commercial Tiles",
+                route: "",
+            },
+            {
+                name: "Residential Tiles",
+                route: "",
+            },
+            {
+                name: "Brick Tiles",
+                route: "",
+            },
+            {
+                name: "All and Others(Tiles)",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "marble",
+        title: "Marble",
+        links: [
+            {
+                name: "Carrara Marble",
+                route: "",
+            },
+            {
+                name: "Calacatta Marble",
+                route: "",
+            },
+            {
+                name: "Statuario Marble",
+                route: "",
+            },
+            {
+                name: "Crema Marfill Marble",
+                route: "",
+            },
+            {
+                name: "Emperador Marble",
+                route: "",
+            },
+            {
+                name: "Nero Marquina Marble",
+                route: "",
+            },
+            {
+                name: "Travertine Marble",
+                route: "",
+            },
+            {
+                name: "Onyx Marble",
+                route: "",
+            },
+            {
+                name: "Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Marble Tiles",
+                route: "",
+            },
+            {
+                name: "All and Others(Marble)",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "granite",
+        title: "Granite",
+        links: [
+            {
+
+            },
+        ],
+    },
+    {
+        id: "sanitary",
+        title: "Sanitary Wares",
+        links: [
+            {
+
+            },
+        ],
+    },
+    {
+        id: "more",
+        title: "More Products",
+        links: [
+            {
+
+            },
+        ],
+    },
+    {
+        id: "company",
+        title: "Company",
+        links: [
+            {
+
+            },
+        ],
+    },
+];
