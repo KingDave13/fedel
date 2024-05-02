2a04e8ca3be6953ecc3c08b33845f4a0423b4e60
David Okaliwe <okaliwedavid@gmail.com>
2024-05-02 14:40:07 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1A1A1A",
+        secondary: "#fea335",
+        main: "#021e31",
+        mainRed: "#E30613",
+        main2: "#dfdfdf",
+        primaryalt: "#f7f6f6",
+        textalt: "#898989",
+        maintext: "#575656",
+        mainalt: "#dddddd",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        'instrument-sans': ["Instrument Sans", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
