3a5aaace55ed95ad0922911942123becaf5f95da
David Okaliwe <okaliwedavid@gmail.com>
2024-05-06 13:34:49 +0100

    Updates

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -13,19 +13,20 @@ const Discover = () => {
         min-h-[900px] flex items-center">
             <div className='relative items-center w-full max-w-[86rem]'>
             <motion.div variants={fadeIn('down', 'spring', 0.3)}
-                className="flex md:flex-row ss:flex-row flex-col md:gap-10 
+                className="flex md:flex-row ss:flex-row flex-col md:gap-20 
                 ss:gap-10 gap-12 w-full aboutimage">
                     <motion.div variants={textVariant()}
-                    className={`${layout.sectionInfo}`}>
+                    className={`${layout.sectionInfo} md:gap-8 ss:gap-8
+                    gap-6`}>
                         <h1 className="text-primary font-bold md:text-[45px]
-                        ss:text-[45px] text-[33px] md:leading-[2px] 
-                        ss:leading-[2px] leading-[40px]">
+                        ss:text-[45px] text-[33px] md:leading-[52px] 
+                        ss:leading-[52px] leading-[40px] tracking-tight">
                         Safe and secure delivery to wherever you are!
                         </h1>
 
-                        <p className='text-main md:text-[17px] ss:text-[15px] 
-                        text-[13px] md:max-w-[700px] ss:max-w-[400px]  
-                        md:leading-[23px] font-medium tracking-tight'>
+                        <p className='text-main md:text-[20px] ss:text-[17px] 
+                        text-[14px] md:max-w-[700px] ss:max-w-[400px]  
+                        md:leading-[28px] font-medium tracking-tight'>
                         Ensuring your satisfaction from start to finish, 
                         Fedel Tiles Limited offers safe and secure 
                         delivery services, no matter where you are. 
@@ -34,10 +35,10 @@ const Discover = () => {
                         arrive on time and in pristine condition.
                         </p>
 
-                        <div className="md:mt-8 ss:mt-8 mt-6">
+                        <div className="">
                             <button className='bg-primary grow5 md:text-[15px] 
-                            ss:text-[15px] text-[14px] md:py-3 ss:py-3 py-2 
-                            md:px-14 ss:px-14 px-6 text-white rounded-lg'
+                            ss:text-[15px] text-[14px] md:py-4 ss:py-3 py-2 
+                            md:px-10 ss:px-10 px-6 text-white rounded-lg'
                             // onClick={() => navigate('/products')}
                             >
                                 Discover Products
@@ -45,10 +46,12 @@ const Discover = () => {
                         </div>
                     </motion.div>
 
-                    <motion.div 
-                        variants={fadeIn('left', 'spring', 0.3)}>
-                            <img src={discoverImg} alt='Discover Image'
-                            className='md:h-[400px] ss:h-[300px] w-auto'
+                    <motion.div variants={fadeIn('left', 'spring', 0.3)}
+                    className="flex-1 w-full">
+                        <img src={discoverImg} 
+                            alt='Discover Image'
+                            className='md:h-[400px] ss:h-[300px] w-full
+                            rounded-3xl'
                         />
                     </motion.div>
                 </motion.div>
