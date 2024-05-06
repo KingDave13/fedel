6c361b35888de1ad31dfe4c0c82888fab2d07762
David Okaliwe <okaliwedavid@gmail.com>
2024-05-06 14:29:55 +0100

    Updates

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -7,33 +7,31 @@ const StepCard = ({ index, title, image, description }) => {
     return (
         <motion.div
         variants={fadeIn('', 'spring', index * 0.5, 0.75)}
-        className='p-10 xs:w-[500px] w-full relative'
-        whileHover={{ boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.2)', 
-        transition: { duration: 0.2, ease: 'easeInOut' } }}
+        className='w-full relative'
         >
             <div className='w-full flex items-center justify-center
             md:gap-6 ss:gap-6 gap-4'>
                 <img 
                     src={image}
                     alt='name'
-                    className='w-10 h-10 object-cover'
+                    className='w-25 h-25 object-cover'
                 />
 
                 <div className='flex flex-col w-full md:gap-3 ss:gap-3
-                gap-2'>
-                    <h1 className='text-primary md:text-[17px] ss:text-[16px] 
-                    text-[13px] font-bold'>
+                gap-2 tracking-tight'>
+                    <h1 className='text-primary md:text-[25px] ss:text-[20px] 
+                    text-[17px] font-bold'>
                         {title}
                     </h1>
 
-                    <p className='text-main font-medium 
-                    md:text-[16px] ss:text-[16px] text-[14px]'>
+                    <p className='text-main font-medium md:leading-[25px]
+                    md:text-[19px] ss:text-[16px] text-[14px]'>
                         {description}
                     </p>
 
-                    <div className='flex gap-2'>
+                    <div className='flex gap-2 cursor-pointer'>
                         <p className='text-primary md:text-[15px] ss:text-[15px]
-                        text-[13px]'>
+                        text-[13px] font-medium'>
                             Browse products
                         </p>
 
@@ -46,10 +44,9 @@ const StepCard = ({ index, title, image, description }) => {
 
 const Steps = () => {
   return (
-    <section className='relative w-full min-h-[900px] mx-auto flex
+    <section className='relative w-full min-h-[700px] mx-auto flex
     items-center'>
-        <div className='max-w-[86rem] mx-auto flex flex-col'
-        >
+        <div className='max-w-[86rem] mx-auto flex flex-col'>
             <motion.div variants={textVariant()}
             className='flex flex-col gap-2'>
                 <h1 className='text-primary font-bold md:text-[45px]
@@ -58,7 +55,7 @@ const Steps = () => {
                 </h1>
                 
                 <p className='text-main font-medium md:text-[20px]
-                ss:text-[20px] text-[15px] tracking-tight md:max-w-[950px]
+                ss:text-[17px] text-[15px] tracking-tight md:max-w-[950px]
                 ss:max-w-[300px]'>
                     You do not need to go through too much stress to find 
                     exactly what you're looking for on our website. We've 
@@ -67,8 +64,8 @@ const Steps = () => {
                 </p>                
             </motion.div>
 
-            <div className='flex gap-6 flex-wrap justify-center md:mt-12
-            ss:mt-12 mt-8'>
+            <div className='grid gap-16 md:mt-24 grid-cols-2 ss:mt-12 
+            mt-8 w-full'>
                 {steps.map((step, index) => (
                     <StepCard 
                         key={step.name} 
@@ -80,7 +77,7 @@ const Steps = () => {
 
             <p className='text-mainalt md:text-[16px] ss:text-[17px] 
             text-[13px] tracking-tight w-full font-medium md:text-center
-            md:mt-8 ss:mt-8 mt-6'>
+            md:mt-14 ss:mt-10 mt-6'>
                 Note that some items may have their prices displayed, 
                 some may not but not to worry, you can request for 
                 their prices directly and you'll be responded to 
