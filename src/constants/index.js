8ea959e10839b450ba1576c7b7978e3a2c750cdb
David Okaliwe <okaliwedavid@gmail.com>
2024-05-06 21:49:41 +0100

    Updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { search, select, place, speed } from '../assets';
+import { search, select, place, speed, twitter, instagram, facebook, whatsapp, youtube, tiktok } from '../assets';
 
 export const navLinks = [
     {
@@ -285,3 +285,288 @@ export const steps = [
         description: "After making your order via WhatsApp or Email, you will be contacted by us immediately on how to take the next steps.",
       },
   ];
+
+  export const socialMedia = [
+    {
+        id: "facebook",
+        image: facebook,
+        link: ""
+    },
+    {
+        id: "instagram",
+        image: instagram,
+        link: ""
+    },
+    {
+        id: "twitter",
+        image: twitter,
+        link: ""
+    },
+    {
+        id: "whatsapp",
+        image: whatsapp,
+        link: ""
+    },
+    {
+        id: "youtube",
+        image: youtube,
+        link: ""
+    },
+    {
+        id: "tiktok",
+        image: tiktok,
+        link: ""
+    },
+  ];
+  
+  export const footerLinks = [
+    {
+      title: "Tiles",
+      links: [
+        {
+          name: "Ceramic Tiles",
+          route: "",
+        },
+        {
+          name: "Porcelain Tiles",
+          route: "",
+        },
+        {
+          name: "Floor Tiles",
+          route: "",
+        },
+        {
+          name: "Wall Tiles",
+          route: "",
+        },
+        {
+          name: "Backsplash Tiles",
+          route: "",
+        },
+        {
+          name: "Bathroom Tiles",
+          route: "",
+        },
+        {
+            name: "Kitchen Tiles",
+            route: "",
+            },
+            {
+            name: "Indoor Tiles",
+            route: "",
+            },
+            {
+            name: "Outdoor Tiles",
+            route: "",
+            },
+            {
+            name: "Pool Tiles",
+            route: "",
+            },
+            {
+            name: "Commercial Tiles",
+            route: "",
+            },
+            {
+            name: "Residential Tiles",
+            route: "",
+            },
+            {
+            name: "Brick Tiles",
+            route: "",
+            },
+            {
+            name: "Other Tiles",
+            route: "",
+            },
+      ],
+    },
+    {
+      title: "Marble",
+      links: [
+        {
+            name: "Carrara Marble",
+            route: "",
+        },
+        {
+            name: "Calacatta Marble",
+            route: "",
+        },
+        {
+            name: "Statuario Marble",
+            route: "",
+        },
+        {
+            name: "Crema Marfil Marble",
+            route: "",
+        },
+        {
+            name: "Emperador Marble",
+            route: "",
+        },
+        {
+            name: "Nero Marquina Marble",
+            route: "",
+        },
+        {
+            name: "Travertine Marble",
+            route: "",
+        },
+        {
+            name: "Onyx Marble",
+            route: "",
+        },
+        {
+            name: "Marble Slabs",
+            route: "",
+        },
+        {
+            name: "Marble Tiles",
+            route: "",
+        },
+      ],
+    },
+
+    {
+        title: "Granite",
+        links: [
+          {
+              name: "Absolute Black Granite",
+              route: "",
+          },
+          {
+              name: "Tan Brown Granite",
+              route: "",
+          },
+          {
+              name: "Giallo Ornamental Granite",
+              route: "",
+          },
+          {
+              name: "Santa Cecilia Granite",
+              route: "",
+          },
+          {
+              name: "Ubatuba Granite",
+              route: "",
+          },
+          {
+              name: "Kashmir White Granite",
+              route: "",
+          },
+          {
+              name: "Baltic Brown Granite",
+              route: "",
+          },
+          {
+              name: "Black Galaxy Granite",
+              route: "",
+          },
+          {
+              name: "Blue Pearl Granite",
+              route: "",
+          },
+          {
+              name: "Tiger Skin Granite",
+              route: "",
+          },
+        ],
+      },
+
+      {
+        title: "Sanitary Wares",
+        links: [
+          {
+              name: "Toilets",
+              route: "",
+          },
+          {
+              name: "Basin/Sinks",
+              route: "",
+          },
+          {
+              name: "Showers",
+              route: "",
+          },
+          {
+              name: "Bidets",
+              route: "",
+          },
+          {
+              name: "Urinals",
+              route: "",
+          },
+          {
+              name: "Faucets and Taps",
+              route: "",
+          },
+          {
+              name: "Accessories",
+              route: "",
+          },
+          {
+              name: "Drainage and Plumbing Fittings",
+              route: "",
+          },
+          {
+              name: "Water Filtration and Purification Systems",
+              route: "",
+          },
+        ],
+      },
+  
+    {
+      title: "More Products",
+      links: [
+        {
+          name: "Marble Slabs",
+          route: "",
+        },
+        {
+          name: "Granite Slabs",
+          route: "",
+        },
+        {
+          name: "Doors",
+          route: "",
+        },
+        {
+          name: "Other Products",
+          route: "",
+        },
+        {
+            name: "Installation Services",
+            route: "",
+        },
+        {
+            name: "Consultation",
+            route: "",
+        },
+      ],
+    },
+    {
+        title: "Company",
+        links: [
+          {
+            name: "About Us",
+            route: "/about",
+          },
+          {
+            name: "Contact Us",
+            route: "/contact",
+          },
+          {
+            name: "Careers",
+            route: "/careers",
+          },
+          {
+            name: "Terms of Usage",
+            route: "/terms",
+          },
+          {
+              name: "Privacy Policy",
+              route: "/privacy",
+          },
+        ],
+      },
+];
--- src/scenes/HomePage.jsx
+++ src/scenes/HomePage.jsx
@@ -29,7 +29,10 @@ const HomePage = () => {
             </div>
             
             <CTA />
-            <Footer />
+
+            <div className='footer'>
+                <Footer />
+            </div>
         </div>
     )
 };
