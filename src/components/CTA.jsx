371474cd410de9c68f9ad1656085ad2befaf03d3
David Okaliwe <okaliwedavid@gmail.com>
2024-05-13 19:10:34 +0100

    Updates

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -17,8 +17,8 @@ const CTA = () => {
             md:gap-6 ss:gap-5 gap-4'>
                 <h2 className='text-white md:text-[28px] ss:text-[20px]
                 text-[18px] tracking-tight font-bold text-center
-                md:max-w-[550px] ss:max-w-[350px] max-w-[200px]
-                md:leading-[35px] ss:leading-[30px] leading-[24px]'>
+                md:max-w-[550px] ss:max-w-[350px] max-w-[250px]
+                md:leading-[35px] ss:leading-[30px] leading-[23px]'>
                     What are you waiting for? Begin exploring our 
                     exquisite array of 
                     <span className='text-secondary'> premium and durable tiles and 
@@ -27,7 +27,7 @@ const CTA = () => {
                 </h2>
 
                 <button className='border-white border-[1px] grow5 
-                md:text-[15px] ss:text-[15px] text-[14px] md:py-3 
+                md:text-[15px] ss:text-[15px] text-[13px] md:py-3 
                 ss:py-3 py-2 md:px-10 ss:px-10 px-6 text-white 
                 rounded-lg'
                 // onClick={() => navigate('/products')}
