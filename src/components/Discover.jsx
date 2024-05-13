ed27178b84124376171510e92540de2cf7884c06
David Okaliwe <okaliwedavid@gmail.com>
2024-05-13 18:48:42 +0100

    Updates

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -10,23 +10,24 @@ const Discover = () => {
 
     return (
         <section className="w-full md:min-h-[550px] ss:min-h-[550px] 
-        min-h-[900px] flex items-center">
+        min-h-[900px] flex items-center md:mt-0 ss:mt-10 mt-96">
             <div className='relative items-center w-full max-w-[86rem]'>
             <motion.div variants={fadeIn('down', 'spring', 0.3)}
                 className="flex md:flex-row ss:flex-row flex-col md:gap-20 
                 ss:gap-10 gap-12 w-full">
                     <motion.div variants={textVariant()}
                     className={`${layout.sectionInfo} md:gap-8 ss:gap-8
-                    gap-6`}>
+                    gap-5`}>
                         <h1 className="text-primary font-bold md:text-[43px]
                         ss:text-[40px] text-[30px] md:leading-[50px] 
-                        ss:leading-[45px] leading-[40px] tracking-tight">
+                        ss:leading-[45px] leading-[35px] tracking-tight">
                             Safe and secure delivery to wherever you are!
                         </h1>
 
                         <p className='text-main md:text-[18px] ss:text-[17px] 
                         text-[14px] md:max-w-[700px] ss:max-w-[400px]  
-                        md:leading-[25px] font-medium tracking-tight'>
+                        md:leading-[25px] ss:leading-[25px] leading-[20px] 
+                        font-medium tracking-tight'>
                             Ensuring your satisfaction from start to finish, 
                             Fedel Tiles Limited offers safe and secure 
                             delivery services, no matter where you are. 
@@ -37,8 +38,8 @@ const Discover = () => {
 
                         <div className="">
                             <button className='bg-primary grow5 md:text-[15px] 
-                            ss:text-[15px] text-[14px] md:py-4 ss:py-3 py-2 
-                            md:px-10 ss:px-10 px-6 text-white rounded-lg'
+                            ss:text-[15px] text-[13px] md:py-4 ss:py-3 py-3 
+                            md:px-10 ss:px-10 px-8 text-white rounded-lg'
                             // onClick={() => navigate('/products')}
                             >
                                 Discover Products
