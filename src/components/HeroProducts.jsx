99181839ab506c18a75cd4f823ef9d8f59d4bfbf
David Okaliwe <okaliwedavid@gmail.com>
2024-05-15 14:10:53 +0100

    Updates

--- src/components/HeroProducts.jsx
+++ src/components/HeroProducts.jsx
@@ -11,10 +11,10 @@ const HeroProducts = () => {
         <div className='relative items-center w-full max-w-[86rem]
         md:mt-28 ss:mt-56 mt-16 rounded-[30px] flex md:p-12 ss:p-10 p-6'
         >
-            <div className='items-center w-full flex flex-col md:gap-5 
+            <div className='w-full flex flex-col md:gap-5 
             ss:gap-5 gap-3'>
                 <motion.div variants={textVariant()}
-                className='justify-center items-center'
+                className='items-center'
                 >
                     <h1 className='text-secondary font-bold md:text-[43px]
                     ss:text-[40px] text-[30px] tracking-tight'>
@@ -23,9 +23,8 @@ const HeroProducts = () => {
                 </motion.div> 
 
                 <motion.div variants={textVariant()}
-                className='justify-center items-center md:gap-3 
-                ss:gap-2 gap-2 text-white md:text-[16px] ss:text-[15px]
-                text-[13px]'
+                className='md:gap-3 ss:gap-2 gap-2 
+                text-white md:text-[16px] ss:text-[15px] text-[13px]'
                 >
                     <p>Home</p>
                     <p>  </p>
