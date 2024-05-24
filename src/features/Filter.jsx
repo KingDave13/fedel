4af398274a61f329d1fdaac028da5f85dbb2e04d
David Okaliwe <okaliwedavid@gmail.com>
2024-05-24 13:05:29 +0100

    Updates

--- src/features/Filter.jsx
+++ src/features/Filter.jsx
@@ -1,71 +1,39 @@
 import { useState, useEffect } from "react";
 import { client } from "../sanity";
 
-const FilterCard = ({ attribute, filterValues, handleFilterChange }) => {
-  return (
-    <div className="mb-6 flex flex-col gap-2">
-      <h3 className="text-main font-bold text-lg">
-        Types
-      </h3>
-
-      <div className="max-h-80 overflow-y-auto flex gap-2">
-          <input
-            type="checkbox"
-          />
-          <label htmlFor="" className="text-main">
-            {attribute.types}
-          </label>
-      </div>
-      
-    </div>
-  );
-};
-
 
 const Filter = ({ products, updateFilteredProducts }) => {
   const [attributes, setAttributes] = useState([]);
   const [filterValues, setFilterValues] = useState({});
 
   useEffect(() => {
-    const fetchAttributes = async () => {
-      try {
-        const query = `*[_type == "attribute"] {
-          _id,
-          type,
-          material,
-          application,
-          styleAndPattern,
-          color,
-          dimensions,
-          manufacturer,
-          price
-        }`;
-        const data = await client.fetch(query);
-        setAttributes(data);
-      } catch (error) {
-        console.error("Error fetching attributes:", error);
-        // Handle error - Show error message or retry fetching
+    const query = `
+      *[_type == "product"] {
+        attributes
       }
-    };
+    `;
 
-    fetchAttributes();
-  }, []);
+    client.fetch(query)
+        .then((data) => setAttributes(data))
+}, []);
 
-  const handleFilterChange = (attribute, value) => {
-    setFilterValues((prevValues) => ({ ...prevValues, [attribute]: value }));
+  const handleFilterChange = (fieldName, value) => {
+    setFilterValues((prevValues) => ({ ...prevValues, [fieldName]: value }));
   };
 
   useEffect(() => {
     const applyFilters = () => {
       const filteredProducts = products.filter((product) => {
-        return Object.keys(filterValues).every((attribute) => {
-          if (filterValues[attribute] === "") return true;
-          const attributeValue = product.attributes.find((attr) => attr._ref === attribute);
+        return Object.keys(filterValues).every((fieldName) => {
+          if (filterValues[fieldName] === false) return true;
+          const attributeValue = product.attributes.find(
+            (attr) => attr._ref === fieldName
+          );
           if (!attributeValue) return false;
           if (Array.isArray(attributeValue.color)) {
-            return attributeValue.color.includes(filterValues[attribute]);
+            return attributeValue.color.includes(filterValues[fieldName]);
           } else {
-            return attributeValue[attribute] === filterValues[attribute];
+            return attributeValue[fieldName] === filterValues[fieldName];
           }
         });
       });
@@ -77,17 +45,18 @@ const Filter = ({ products, updateFilteredProducts }) => {
 
   return (
     <section>
-      <div className="flex flex-col w-full">
-        {attributes.map((attribute) => (
-          <FilterCard 
-            key={attribute._id} 
-            attribute={attribute} 
-            filterValues={filterValues} 
-            handleFilterChange={handleFilterChange} 
-          />
-        ))}
+    <div className="flex flex-col w-full">
+      {attributes.map((attribute) => (
+        <div>
+        <h3>{attribute.name}</h3>
+        <p>{attribute.title}</p>
       </div>
-    </section>
+    
+      ))}
+    </div>
+  </section>
+
+
   );
 };
 
