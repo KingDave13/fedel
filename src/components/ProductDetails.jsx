d2affe778a0d7318115f15c1ee4a97619739a5d0
David Okaliwe <okaliwedavid@gmail.com>
2024-07-11 17:26:09 +0100

    Updates

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -218,9 +218,43 @@ const RequestModal = ({ onClose, product, image }) => {
                                             bg-transparent w-full custom-select"
                                         >
                                             <option value="" disabled selected hidden>Select your state of residence</option>
-                                            <option value="social_media">Social Media</option>
-                                            <option value="from_friend">From a friend</option>
-                                            <option value="other">Other</option>
+                                            <option value="fct">FCT</option>
+                                            <option value="abia">Abia</option>
+                                            <option value="adamawa">Adamawa</option>
+                                            <option value="akwaibom">Akwa Ibom</option>
+                                            <option value="anambra">Anambra</option>
+                                            <option value="bauchi">Bauchi</option>
+                                            <option value="bayelsa">Bayelsa</option>
+                                            <option value="benue">Benue</option>
+                                            <option value="borno">Borno</option>
+                                            <option value="crossriver">Cross River</option>
+                                            <option value="delta">Delta</option>
+                                            <option value="ebonyi">Ebonyi</option>
+                                            <option value="edo">Edo</option>
+                                            <option value="ekiti">Ekiti</option>
+                                            <option value="enugu">Enugu</option>
+                                            <option value="gombe">Gombe</option>
+                                            <option value="imo">Imo</option>
+                                            <option value="jigawa">Jigawa</option>
+                                            <option value="kaduna">Kaduna</option>
+                                            <option value="kano">Kano</option>
+                                            <option value="katsina">Katsina</option>
+                                            <option value="kebbi">Kebbi</option>
+                                            <option value="kogi">Kogi</option>
+                                            <option value="kwara">Kwara</option>
+                                            <option value="lagos">Lagos</option>
+                                            <option value="nasarawa">Nasarawa</option>
+                                            <option value="niger">Niger</option>
+                                            <option value="ogun">Ogun</option>
+                                            <option value="ondo">Ondo</option>
+                                            <option value="osun">Osun</option>
+                                            <option value="oyo">Oyo</option>
+                                            <option value="plateau">Plateau</option>
+                                            <option value="rivers">Rivers</option>
+                                            <option value="sokoto">Sokoto</option>
+                                            <option value="taraba">Taraba</option>
+                                            <option value="yobe">Yobe</option>
+                                            <option value="zamfara">Zamfara</option>
                                         </select>
                                         <div className='absolute md:right-3 
                                         ss:right-3 right-2'>
