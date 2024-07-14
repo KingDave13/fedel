70b86652c67699aa89c58ade58a3002ec6c9e9d3
David Okaliwe <okaliwedavid@gmail.com>
2024-07-14 19:43:02 +0100

    Updates

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,195 +1,139 @@
+import { useState, useEffect, useRef } from 'react';
+import { SectionWrapper } from '../hoc';
 import { motion } from 'framer-motion';
-import { fadeIn } from '../utils/motion';
-import { SectionWrapperAlt } from '../hoc';
-import { useSelector, useDispatch } from 'react-redux';
-import { removeFromCart, clearCart, incrementQuantity, decrementQuantity } from '../redux/cartSlice';
+import { textVariant } from '../utils/motion';
+import { client, urlFor } from '../sanity';
+import { CgArrowLeft } from "react-icons/cg";
 import { Link } from 'react-router-dom';
-import { add, subtract, trash } from '../assets';
-import { urlFor } from '../sanity';
 
-const ItemCard = ({ item, index, image }) => {
-    const imageUrl = urlFor(image).url();
-    const dispatch = useDispatch();
+const ImageCard = ({ image, name, slug, categorySlug }) => {
+    const imageUrl = image ? urlFor(image).url() : '';
 
     return (
-        <motion.div variants={fadeIn('', 'spring', index * 0.5, 0.75)}>
-            <div className='bg-main2 rounded-2xl p-7 flex flex-col gap-3'>
-                <div className='flex w-full justify-between'>
-                    <div className='flex gap-6'>
+        <div className='cursor-pointer grow2'>
+            <a href={`/products/${categorySlug}/${slug}`}>
+                {imageUrl ? (
+                    <div className='w-[200px] h-[200px]'>
                         <img 
                             src={imageUrl}
-                            alt={item.name}
-                            className='rounded-xl w-32 h-32 object-cover'
-                        />
-
-                        <div className='flex flex-col gap-1'>
-                            <h2 className='text-main font-bold text-[20px]'>
-                                {item.name}
-                            </h2>
-                            <p className='text-mainalt text-[16px]'>
-                                {item.type}
-                            </p>
-                            <p className='text-mainalt text-[16px]'>
-                                {item.manufacturer}
-                            </p>
-                            <p className='text-main font-semibold 
-                            text-[14px] flex flex-wrap gap-3'>
-                                {item.variations.map((variation, index) => (
-                                    <span key={index}>
-                                        {variation}
-                                    </span>
-                                ))}
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className='flex flex-col gap-1 text-right'>
-                        <h1 className='text-greenDeep text-[22px] 
-                        font-bold'>
-                            <div>
-                                <span className='line-through'>
-                                    N
-                                </span>
-                                {item.price}.00
-                            </div>
-                        </h1>
-
-                        <h1 className='text-main3 font-medium text-[18px] 
-                        line-through'>
-                            <div>
-                                N{item.OriginalPrice}.00
-                            </div>
-                        </h1>
-                    </div>
-                </div>
-
-                <div className='flex w-full justify-between items-center'>
-                    <div className='flex items-center gap-1 cursor-pointer
-                    grow2'
-                    onClick={() => dispatch(removeFromCart(item.id))}
-                    >
-                        <img
-                            src={trash}
-                            alt='delete'
-                            className='text-brightRed w-6 h-6'
-                        />
-                        <p className='text-main text-[13px] mt-1'>
-                            Remove Item
-                        </p>
-                    </div>
-
-                    <div className='flex items-center gap-3'>
-                        <img
-                            src={add}
-                            alt='add'
-                            className='cursor-pointer grow2 w-7 h-7'
-                            onClick={() => dispatch(incrementQuantity(item.id))}
-                        />
-
-                        <p className='text-primary font-semibold text-[14px]'>
-                            {item.quantity}
-                        </p>
-
-                        <img
-                            src={subtract}
-                            alt='subtract'
-                            className='cursor-pointer grow2 w-7 h-7'
-                            onClick={() => dispatch(decrementQuantity(item.id))}
+                            alt={name}
+                            className='rounded-lg object-cover w-full 
+                            h-full'
                         />
                     </div>
-                </div>
-            </div>
-        </motion.div>
+                ) : (
+                    <div className='flex items-center justify-center 
+                    bg-gray-200 rounded-lg w-[200px] h-[200px]'>
+                        <span>No Image</span>
+                    </div>
+                )}
+            </a>
+        </div>
     );
 };
 
-const Cart = () => {
-    const cartItems = useSelector((state) => state.cart.items);
-    const itemCount = cartItems.length;
-    const totalAmount = cartItems.reduce((total, item) => total + (parseFloat(item.price.replace(',', '')) * item.quantity), 0);
-    const dispatch = useDispatch();
+const MoreProducts = () => {
+    const [moreProducts, setMoreProducts] = useState([]);
+    const carouselRef = useRef(null);
+    const itemRef = useRef(null);
+
+    useEffect(() => {
+        const query = `
+            *[_type == "product"] | order(_createdAt desc) [0...50] {
+                _id,
+                name,
+                images,
+                slug,
+                "categorySlug": category->slug.current
+            }
+        `;
+        
+        client.fetch(query)
+            .then((data) => {
+                const shuffledProducts = data.sort(() => 0.5 - Math.random()).slice(0, 10);
+                setMoreProducts(shuffledProducts);
+            })
+            .catch((error) => console.error('Error fetching random products:', error));
+    }, []);
+
+    const scrollLeft = () => {
+        if (carouselRef.current && itemRef.current) {
+            const itemWidth = itemRef.current.clientWidth + 24;
+            carouselRef.current.scrollBy({
+                left: -itemWidth,
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    const scrollRight = () => {
+        if (carouselRef.current && itemRef.current) {
+            const itemWidth = itemRef.current.clientWidth + 24;
+            carouselRef.current.scrollBy({
+                left: itemWidth,
+                behavior: 'smooth'
+            });
+        }
+    };
 
     return (
-        <section className='relative w-full min-h-[60px] mx-auto flex
-        items-center'>
-            <div className='max-w-[86rem] mx-auto flex flex-col md:gap-8
-            ss:gap-8 gap-6 w-full'>
-                <h1 className='text-primary font-bold md:text-[22px]
-                ss:text-[20px] text-[16px]'>
-                    Cart {`(${itemCount})`}
-                </h1>
-                
-                {cartItems.length === 0 ? (
-                    <p className='text-primary font-medium text-[17px]'>
-                        No items in cart
-                    </p>
-                ) : (
-                    <div className='w-full flex gap-6'>
-                        <div className='flex flex-col w-full gap-6'>
-                            {cartItems.map((item, index) => (
-                                <ItemCard 
-                                    key={item.id}
-                                    item={item}
-                                    index={index}
-                                    image={item.image}
-                                />
+        <section className='relative w-full md:min-h-[300px] 
+        ss:min-h-[300px] min-h-[800px] mx-auto flex items-center'>
+            <div className='max-w-[86rem] mx-auto flex flex-col w-full 
+            md:gap-10'>
+                <motion.div variants={textVariant()}>
+                    <h1 className='text-primary font-bold md:text-[30px] 
+                    ss:text-[30px] text-[20px] tracking-tight'>
+                        Explore more products
+                    </h1>
+                </motion.div>
+
+                <div className='relative flex items-center'>
+                    <button 
+                        className='absolute left-6 z-10 bg-main 
+                        text-white p-3 rounded-full opacity-90 
+                        hover:opacity-100'
+                        onClick={scrollLeft}
+                    >
+                        <CgArrowLeft size={18} />
+                    </button>
+                    <div className='overflow-hidden w-full flex' ref={carouselRef}>
+                        <div className='flex gap-6'>
+                            {moreProducts.map((item, index) => (
+                                <div key={item._id} ref={index === 0 ? itemRef : null}>
+                                    <ImageCard 
+                                        image={item.images && item.images[0]}
+                                        name={item.name}
+                                        slug={item.slug.current}
+                                        categorySlug={item.categorySlug}
+                                    />
+                                </div>
                             ))}
                         </div>
-                        
-                        <div>
-                            <div className='bg-main2 p-7 flex flex-col gap-3
-                            rounded-2xl'>
-                                <h2 className='text-main font-bold 
-                                text-[18px]'>
-                                    Cart Summary
-                                </h2>
-
-                                <div className='flex w-full justify-between
-                                items-center'>
-                                    <p className='text-mainalt text-[16px]'>
-                                        Subtotal:
-                                    </p>
-
-                                    <p className='text-greenDeep md:text-[20px] 
-                                    font-bold'>
-                                        <span className='line-through'>
-                                            N
-                                        </span>
-                                        {totalAmount.toLocaleString()}.00
-                                    </p>
-                                </div>
-                                
-                                <p className='text-main3 text-[14px]
-                                border-t-[1px] border-primaryalt pt-4 mt-2'>
-                                    This figure does not include any other 
-                                    extra fees that may be incurred via 
-                                    logistics, etc.
-                                </p>
-
-                                <Link to='/cart/checkout' 
-                                className='bg-primary text-[14px] py-3.5 flex
-                                items-center justify-center text-white rounded-lg 
-                                grow2 cursor-pointer w-full mt-3'>
-                                    <p>
-                                        Checkout
-                                    </p>
-                                </Link>
-                            </div>
-                        </div>
                     </div>
-                )}
-
-                <div className='mt-6'>
-                    <button onClick={() => dispatch(clearCart())}
-                    className='text-white bg-primary py-2 px-10 grow2
-                    rounded-lg text-[14px]'>
-                        Clear Cart
+                    <button 
+                        className='absolute right-6 z-10 bg-main 
+                        text-white p-3 rounded-full opacity-90 
+                        hover:opacity-100'
+                        onClick={scrollRight}
+                    >
+                        <CgArrowLeft size={18} className="transform
+                         rotate-180" />
                     </button>
                 </div>
                 
+                <div className='w-full flex items-center justify-center'>
+                    <Link to={`/products`}
+                        className='bg-primary text-[14px] py-3.5 
+                        text-center text-white rounded-lg grow2 
+                        cursor-pointer w-[180px]'
+                    >
+                        See more products
+                    </Link>
+                </div>
             </div>
         </section>
     );
 };
 
-export default SectionWrapperAlt(Cart, '');
+export default SectionWrapper(MoreProducts, '');
