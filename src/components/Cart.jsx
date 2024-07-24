d6c286b1bde99474ce74a342fc7bbd8a6c846a44
David Okaliwe <okaliwedavid@gmail.com>
2024-07-24 19:44:09 +0100

    Updates

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -14,26 +14,34 @@ const ItemCard = ({ item, index, image }) => {
         <motion.div variants={fadeIn('', 'spring', index * 0.5, 0.75)}>
             <div className='bg-main2 md:rounded-2xl ss:rounded-2xl 
             rounded-xl md:p-7 ss:p-7 p-4 flex flex-col gap-3'>
-                <div className='flex w-full justify-between'>
-                    <div className='flex gap-6'>
+                <div className='flex md:flex-row flex-col w-full 
+                justify-between'>
+                    <div className='flex md:gap-6 ss:gap-6 gap-3'>
                         <img 
                             src={imageUrl}
                             alt={item.name}
-                            className='rounded-xl w-32 h-32 object-cover'
+                            className='md:rounded-xl ss:rounded-xl
+                            rounded-lg md:w-32 ss:w-32 w-20 md:h-32 ss:h-32 
+                            h-20 object-cover'
                         />
 
-                        <div className='flex flex-col gap-1'>
-                            <h2 className='text-main font-bold text-[20px]'>
+                        <div className='flex flex-col md:gap-1 ss:gap-1
+                        gap-0.5'>
+                            <h2 className='text-main font-bold md:text-[20px]
+                            ss:text-[20px] text-[15px]'>
                                 {item.name}
                             </h2>
-                            <p className='text-mainalt text-[16px]'>
+                            <p className='text-mainalt md:text-[16px]
+                            ss:text-[16px] text-[12px]'>
                                 {item.type}
                             </p>
-                            <p className='text-mainalt text-[16px]'>
+                            <p className='text-mainalt md:text-[16px]
+                            ss:text-[16px] text-[12px]'>
                                 {item.manufacturer}
                             </p>
                             <p className='text-main font-semibold 
-                            text-[14px] flex flex-wrap gap-3'>
+                            md:text-[14px] ss:text-[14px] text-[12px] 
+                            flex flex-wrap md:gap-3 ss:gap-3 gap-2'>
                                 {item.variations.map((variation, index) => (
                                     <span key={index}>
                                         {variation}
@@ -43,9 +51,10 @@ const ItemCard = ({ item, index, image }) => {
                         </div>
                     </div>
 
-                    <div className='flex flex-col gap-1 text-right'>
-                        <h1 className='text-greenDeep text-[22px] 
-                        font-bold'>
+                    <div className='flex flex-col md:gap-1 gap-0 
+                    md:text-right md:mt-0 ss:mt-3 mt-3'>
+                        <h1 className='text-greenDeep md:text-[22px] 
+                        ss:text-[21px] text-[17px] font-bold'>
                             <div>
                                 <span className='line-through'>
                                     N
@@ -54,8 +63,8 @@ const ItemCard = ({ item, index, image }) => {
                             </div>
                         </h1>
 
-                        <h1 className='text-main3 font-medium text-[18px] 
-                        line-through'>
+                        <h1 className='text-main3 font-medium md:text-[18px] 
+                        ss:text-[17px] text-[14px] line-through'>
                             <div>
                                 N{item.OriginalPrice}.00
                             </div>
@@ -71,18 +80,22 @@ const ItemCard = ({ item, index, image }) => {
                         <img
                             src={trash}
                             alt='delete'
-                            className='text-brightRed w-6 h-6'
+                            className='text-brightRed md:w-6 ss:w-6 w-5 
+                            md:h-6 ss:h-6 h-5'
                         />
-                        <p className='text-main text-[13px] mt-1'>
+                        <p className='text-main md:text-[13px] ss:text-[13px]
+                        text-[12px] mt-1'>
                             Remove Item
                         </p>
                     </div>
 
-                    <div className='flex items-center gap-3'>
+                    <div className='flex items-center md:gap-3 ss:gap-3
+                    gap-2.5'>
                         <img
                             src={add}
                             alt='add'
-                            className='cursor-pointer grow2 w-7 h-7'
+                            className='cursor-pointer grow2 md:w-7 ss:w-7 
+                            w-5 md:h-7 ss:h-7 h-5'
                             onClick={() => dispatch(incrementQuantity(item.id))}
                         />
 
@@ -93,7 +106,8 @@ const ItemCard = ({ item, index, image }) => {
                         <img
                             src={subtract}
                             alt='subtract'
-                            className='cursor-pointer grow2 w-7 h-7'
+                            className='cursor-pointer grow2 md:w-7 ss:w-7 
+                            w-5 md:h-7 ss:h-7 h-5'
                             onClick={() => dispatch(decrementQuantity(item.id))}
                         />
                     </div>
@@ -111,7 +125,7 @@ const Cart = () => {
 
     return (
         <section className='relative w-full min-h-[60px] mx-auto flex
-        items-center'>
+        items-center md:px-0 ss:px-16 px-6 md:py-0 ss:py-0 py-3'>
             <div className='max-w-[86rem] mx-auto flex flex-col md:gap-8
             ss:gap-8 gap-6 w-full'>
                 <h1 className='text-primary font-bold md:text-[22px]
@@ -126,7 +140,7 @@ const Cart = () => {
                 ) : (
                     <div className='w-full flex md:flex-row ss:flex-row 
                     flex-col gap-6'>
-                        <div className='flex flex-col w-full md:gap-6\
+                        <div className='flex flex-col w-full md:gap-6
                         ss:gap-6 gap-4'>
                             {cartItems.map((item, index) => (
                                 <ItemCard 
@@ -166,7 +180,8 @@ const Cart = () => {
                                 <p className='text-main3 md:text-[14px]
                                 ss:text-[14px] text-[11px] border-t-[1px] 
                                 border-primaryalt md:pt-4 ss:pt-4 pt-3 
-                                md:mt-2 ss:mt-2 mt-1'>
+                                md:mt-2 ss:mt-2 mt-1 md:leading-0
+                                ss:leading-[20px] leading-[18px]'>
                                     This figure does not include any other 
                                     extra fees that may be incurred via 
                                     logistics, etc.
