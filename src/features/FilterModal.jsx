149b2ef46e6fb237971a80f37f751fdaefde9527
David Okaliwe <okaliwedavid@gmail.com>
2024-07-27 13:33:19 +0100

    Updates

--- src/features/FilterModal.jsx
+++ src/features/FilterModal.jsx
@@ -1,4 +1,5 @@
 import { motion, AnimatePresence } from 'framer-motion';
+import { filter } from '../assets';
 
 const FilterModal = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
@@ -17,11 +18,27 @@ const FilterModal = ({ isOpen, onClose, children }) => {
                         animate={{ y: 0, opacity: 1 }}
                         exit={{ y: 10, opacity: 0 }}
                         transition={{ duration: 0.1 }}
-                        className="bg-white ss:p-8 p-4 padphone rounded-2xl 
+                        className="bg-white padphone
                         shadow-xl flex flex-col ss:justify-center 
-                        w-full ss:h-auto h-[80%] overflow-auto 
+                        w-full ss:h-auto h-[75%] overflow-auto 
                         items-center relative ss:m-16 m-6">
-                            {children}
+                            <div className='flex bg-main2 w-full 
+                            top-0 ss:p-8 p-4 items-center gap-4 phone3'>
+                                <img 
+                                    src={filter}
+                                    alt="filter"
+                                    className="w-5"
+                                />
+                                <p className="text-main font-semibold
+                                ss:text-[16px] text-[15px]">
+                                    Filters
+                                </p>
+                            </div>
+
+                            <div className='ss:p-8 p-4 w-full'>
+                                {children}
+                            </div>
+                            
                     </motion.div>
                 </motion.div>
             )}
