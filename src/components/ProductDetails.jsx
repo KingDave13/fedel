126995e29a90de4cdcea45f6fefd9eea32506604
David Okaliwe <okaliwedavid@gmail.com>
2024-08-07 22:56:40 +0100

    Updates

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -703,7 +703,6 @@ const ProductDetails = ({ product }) => {
                                                 <div>
                                                     <span className='line-through'>
                                                         N
-                                                        
                                                     </span>
                                                     {attribute.price}.00
                                                 </div>
