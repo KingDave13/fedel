3ccfa407328e6933a7c3119b8e817d849f6730a1
David Okaliwe <okaliwedavid@gmail.com>
2024-08-15 23:28:35 +0100

    Updates

--- src/constants/index.js
+++ src/constants/index.js
@@ -309,6 +309,345 @@ export const navLinks = [
     },
 ];
 
+export const searchLinks = [
+    {
+        id: "tiles",
+        title: "Tiles",
+        links: [
+            {
+                name: "Ceramic Tiles",
+                route: "",
+            },
+            {
+                name: "Porcelain Tiles",
+                route: "",
+            },
+            {
+                name: "Floor Tiles",
+                route: "",
+            },
+            {
+                name: "Wall Tiles",
+                route: "",
+            },
+            {
+                name: "Backsplash Tiles",
+                route: "",
+            },
+            {
+                name: "Bathroom Tiles",
+                route: "",
+            },
+            {
+                name: "Kitchen Tiles",
+                route: "",
+            },
+            {
+                name: "Indoor Tiles",
+                route: "",
+            },
+            {
+                name: "Outdoor Tiles",
+                route: "",
+            },
+            {
+                name: "Pool Tiles",
+                route: "",
+            },
+            {
+                name: "Commercial Tiles",
+                route: "",
+            },
+            {
+                name: "Residential Tiles",
+                route: "",
+            },
+            {
+                name: "Brick Tiles",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "marble",
+        title: "Marble",
+        links: [
+            {
+                name: "Carrara Marble",
+                route: "",
+            },
+            {
+                name: "Calacatta Marble",
+                route: "",
+            },
+            {
+                name: "Statuario Marble",
+                route: "",
+            },
+            {
+                name: "Crema Marfill Marble",
+                route: "",
+            },
+            {
+                name: "Emperador Marble",
+                route: "",
+            },
+            {
+                name: "Nero Marquina Marble",
+                route: "",
+            },
+            {
+                name: "Travertine Marble",
+                route: "",
+            },
+            {
+                name: "Onyx Marble",
+                route: "",
+            },
+            {
+                name: "Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Marble Tiles",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "granite",
+        title: "Granite",
+        links: [
+            {
+                name: "Absolute Black Granite",
+                route: "",
+            },
+            {
+                name: "Tan Brown Granite",
+                route: "",
+            },
+            {
+                name: "Giallo Ornamental Granite",
+                route: "",
+            },
+            {
+                name: "Santa Cecilia Granite",
+                route: "",
+            },
+            {
+                name: "Ubatuba Granite",
+                route: "",
+            },
+            {
+                name: "Kashmir White Granite",
+                route: "",
+            },
+            {
+                name: "Baltic Brown Granite",
+                route: "",
+            },
+            {
+                name: "Black Galaxy Granite",
+                route: "",
+            },
+            {
+                name: "Blue Pearl Granite",
+                route: "",
+            },
+            {
+                name: "Tiger Skin Granite",
+                route: "",
+            },
+            {
+                name: "Granite Slabs",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "marbleslab",
+        title: "Marble Slab",
+        links: [
+            {
+                name: "Carrara Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Calacatta Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Statuario Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Nero Marquina Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Crema Marfil Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Emperador Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Travertine Marble Slabs",
+                route: "",
+            },
+            {
+                name: "Onyx Marble Slabs",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "graniteslab",
+        title: "Granite Slab",
+        links: [
+            {
+                name: "Absolute Black Granite Slabs",
+                route: "",
+            },
+            {
+                name: "Tan Brown Granite Slabs",
+                route: "",
+            },
+            {
+                name: "Giallo Ornamental Granite Slabs",
+                route: "",
+            },
+            {
+                name: "Santa Cecilia Granite Slabs",
+                route: "",
+            },
+            {
+                name: "Blue Pearl Granite Slabs",
+                route: "",
+            },
+            {
+                name: "Black Galaxy Granite Slabs",
+                route: "",
+            },
+            {
+                name: "Kashmir White Granite Slabs",
+                route: "",
+            },
+            {
+                name: "Baltic Brown Granite Slabs",
+                route: "",
+            },
+            {
+                name: "Ubatuba Granite Slabs",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "sanitary",
+        title: "Sanitary Wares",
+        links: [
+            {
+                name: "Toilets",
+                route: "",
+            },
+            {
+                name: "Basins/Sinks",
+                route: "",
+            },
+            {
+                name: "Showers",
+                route: "",
+            },
+            {
+                name: "Bidets",
+                route: "",
+            },
+            {
+                name: "Urinals",
+                route: "",
+            },
+            {
+                name: "Faucets and Taps",
+                route: "",
+            },
+            {
+                name: "Accessories",
+                route: "",
+            },
+            {
+                name: "Drainage and Plumbing Fittings",
+                route: "",
+            },
+            {
+                name: "Water Filtration/Purification Systems",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "floorandwall",
+        title: "Floor and Wall Materials",
+        links: [
+            {
+                name: "Hardwood",
+                route: "",
+            },
+            {
+                name: "Laminate",
+                route: "",
+            },
+            {
+                name: "Vinyl",
+                route: "",
+            },
+            {
+                name: "Carpet",
+                route: "",
+            },
+            {
+                name: "Urinals",
+                route: "",
+            },
+            {
+                name: "Paint",
+                route: "",
+            },
+            {
+                name: "Wallpaper",
+                route: "",
+            },
+            {
+                name: "Wood Panelling",
+                route: "",
+            },
+            {
+                name: "Wainscoting",
+                route: "",
+            },
+        ],
+    },
+    {
+        id: "more",
+        title: "More Products",
+        links: [
+            {
+                name: "Doors",
+                route: "",
+            },
+            {
+                name: "Installation Services",
+                route: "",
+            },
+            {
+                name: "Consultation",
+                route: "",
+            },
+        ],
+    },
+];
+
 export const steps = [
     {
       image: search,
@@ -319,20 +658,20 @@ export const steps = [
         image: select,
         title: "Select a product",
         description: "After finding the type or style of tile/other item you're looking for, select to show more details about the product and make your order/inquiry.",
-      },
-      {
+    },
+    {
         image: place,
         title: "Place your order",
         description: "After selecting an available item, you can place your order via WhatsApp or Email and you'll be responded to swiftly and how to move forward.",
-      },
-      {
+    },
+    {
         image: speed,
         title: "We will be with you speedily!",
         description: "After making your order via WhatsApp or Email, you will be contacted by us immediately on how to take the next steps.",
-      },
-  ];
+    },
+];
 
-  export const socialMedia = [
+export const socialMedia = [
     {
         id: "facebook",
         image: facebook,
