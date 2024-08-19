26158ce067587228403d7949600fd2a213611ccf
David Okaliwe <okaliwedavid@gmail.com>
2024-08-19 20:38:39 +0100

    Updates

--- src/features/FilterModal.jsx
+++ src/features/FilterModal.jsx
@@ -27,7 +27,7 @@ const FilterModal = ({ isOpen, onClose, children, handleSaveAndRefresh }) => {
                     shadow-xl flex flex-col ss:justify-center 
                     w-full ss:h-auto h-[100%] overflow-auto 
                     items-center relative ss:m-16">
-                        <div className='flex bg-main2 w-full z-20
+                        <div className='flex bg-main2 w-full z-10
                         top-0 ss:p-8 p-4 items-center gap-3 phone3'>
                             <img 
                                 src={filter}
