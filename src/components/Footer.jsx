da4c97a6d7de93978c34cf22f6dab60aa137e453
David Okaliwe <okaliwedavid@gmail.com>
2024-09-23 20:52:34 +0100

    Updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,22 +8,22 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <section className='relative w-full md:min-h-[800px] ss:min-h-[700px] 
+    <section className='relative w-full md:min-h-[700px] ss:min-h-[700px] 
     min-h-[450px] flex items-center flex-col'>
       <div className='flex items-center w-full relative'>
         <div className='flex flex-col w-full'>
-          <div className='flex flex-col w-full md:gap-6 ss:gap-5 gap-5'>
-            <img src={logoalt} alt='logo' className='md:w-[200px] 
+          <div className='flex flex-col w-full md:gap-5 ss:gap-5 gap-5'>
+            <img src={logoalt} alt='logo' className='md:w-[150px] 
             ss:w-[180px] w-[160px] h-auto' />
 
-            <p className='text-white md:text-[15px] ss:text-[15px] 
+            <p className='text-white md:text-[14px] ss:text-[15px] 
             text-[12px] md:leading-[25px] ss:leading-[25px] leading-[18px]'>
               Shoptiles.ng is a registered company and certified
               retailer of tiles and other home/sanitation materials in
               Nigeria.
             </p>
             
-            <div className='flex md:gap-5 ss:gap-5 gap-3 items-center'>
+            <div className='flex md:gap-4 ss:gap-5 gap-3 items-center'>
                 {socialMedia.map((social, index) => (
                     <a target='_blank' href={social.link} rel="noreferrer" key={index}>
                         <img src={social.image} alt='social'
@@ -34,12 +34,12 @@ const Footer = () => {
             </div>
           </div>
           
-          <div className='w-full flex md:flex-row flex-col md:mt-8 
+          <div className='w-full flex md:flex-row flex-col md:mt-6 
           ss:mt-8 mt-6'>
             {footerLinks.map((footerLink, index) => (
-              <div key={index} className='flex flex-col md:my-4 ss:my-4
+              <div key={index} className='flex flex-col md:my-3 ss:my-4
               my-3 w-full'>
-                <h4 className={`font-bold md:text-[17px] ss:text-[17px] 
+                <h4 className={`font-bold md:text-[15px] ss:text-[17px] 
                 text-[14px] text-white 
                   ${index !== footerLinks.length - 1 ? 'md:mr-10 ss:mr-8 mr-8' : 'mr-12'}`}>
                   {footerLink.title}
@@ -49,8 +49,8 @@ const Footer = () => {
                 justify-between flex flex-col'>
                   {footerLink.links.map((Link, index) => (
                     <a target='blank' href={Link.route} key={Link.name}>
-                      <li className={`md:text-[14px] ss:text-[15px] grow2
-                      text-[12px] md:leading-[22px] ss:leading-[20px] leading-[12px]
+                      <li className={`md:text-[13px] ss:text-[15px] grow2
+                      text-[12px] md:leading-[19px] ss:leading-[20px] leading-[12px]
                       text-white hover:text-secondary cursor-pointer 
                       ${index !== footerLink.links.length - 1 ? 'md:mb-2.5 ss:mb-2 mb-2.5' : 'mb-0'}`}>
                         {Link.name}
@@ -65,12 +65,12 @@ const Footer = () => {
       </div>
 
       <motion.div variants={textVariant()} className='flex flex-col w-full'>
-        <div className='flex md:mt-16 ss:mt-10 mt-8 border-t-[1px] 
-        border-main md:pt-10 ss:pt-8 pt-4 items-center'>
-          <BiCopyright className='sm:mr-2 mr-1 md:text-[20px] 
+        <div className='flex md:mt-8 ss:mt-10 mt-8 border-t-[1px] 
+        border-main md:pt-6 ss:pt-8 pt-4 items-center'>
+          <BiCopyright className='sm:mr-1 mr-1 md:text-[16px] 
           ss:text-[18px] text-[15px] mt-1 text-white' />
 
-          <p className='md:text-[14px] ss:text-[16px] text-[12px] 
+          <p className='md:text-[13px] ss:text-[16px] text-[12px] 
           text-white md:mt-1 ss:mt-1 mt-0.5'>
             2024 Shoptiles.ng. All Rights Reserved.
           </p>
