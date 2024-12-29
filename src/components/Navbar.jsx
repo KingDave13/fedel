c554ba596e3643cd03d4b256c9b9b8d80bfa4dfe
David Okaliwe <okaliwedavid@gmail.com>
2024-12-29 02:36:50 +0000

    Updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect, useRef } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { BsX } from 'react-icons/bs';
 import styles from '../styles';
-import { HiOutlineChat } from 'react-icons/hi';
+import { HiOutlineChat, HiX } from 'react-icons/hi';
 import { navLinks } from '../constants';
 import { google, logo } from '../assets';
 import { TiArrowSortedDown } from "react-icons/ti";
@@ -9,9 +10,306 @@ import { RiWhatsappLine } from "react-icons/ri";
 import { PiLineVerticalThin } from "react-icons/pi";
 import { IoCartOutline, IoSearchOutline, IoMenu } from "react-icons/io5";
 import { FiMail } from "react-icons/fi";
+import { GrAttachment } from "react-icons/gr";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 import { useSelector } from 'react-redux';
 import { client } from '../sanity';
 import { useNavigate, useParams } from 'react-router-dom';
+
+
+const QuoteModal = ({ onClose }) => {
+    const [files, setFiles] = useState([]);
+    const [previews, setPreviews] = useState([]);
+
+    const closeQuoteModal = () => {
+        onClose();
+        document.body.style.overflow = 'auto';
+        document.body.style.top = '0';
+    }
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            subject: '',
+            message: '',
+        },
+        validationSchema: Yup.object().shape({
+            name: Yup.string().required('Name is required.'),
+            email: Yup.string().email('Invalid email address.').required('Email is required.'),
+            subject: Yup.string().required('Subject is required.'),
+            message: Yup.string().required('Message is required.'),
+        }),
+        onSubmit: (values) => {
+
+        },
+    });
+    
+    const handleFileChange = (e) => {
+        const selectedFiles = Array.from(e.target.files);
+        const validFiles = selectedFiles.filter(file =>
+            (file.type === 'image/jpeg' || file.type === 'image/png') && file.size <= 2 * 1024 * 1024 // 2MB
+        );
+    
+        if (validFiles.length !== selectedFiles.length) {
+            alert('Some files are invalid. Only JPEG, JPG, and PNG files less than 2MB are allowed.');
+        }
+    
+        // Generate previews
+        const filePreviews = validFiles.map(file => URL.createObjectURL(file));
+        setFiles(validFiles);
+        setPreviews(filePreviews);
+    };
+
+    useEffect(() => {
+        // Cleanup function to revoke object URLs
+        return () => {
+            previews.forEach(preview => URL.revokeObjectURL(preview));
+        };
+    }, [previews]);
+
+    const handleWhatsAppSend = () => {
+        formik.validateForm().then((errors) => {
+            if (Object.keys(errors).length > 0) {
+                formik.setTouched({
+                    name: true,
+                    email: true,
+                    subject: true,
+                    message: true,
+                });
+                alert("Please complete all required form fields.");
+                return;
+            }
+
+            const formDataText = `Name: ${formik.values.name}\nEmail: ${formik.values.email}\nSubject: ${formik.values.subject}\nMessage: ${formik.values.message}`;
+            const whatsappLink = `https://wa.me/2349014452743?text=${encodeURIComponent(formDataText)}`;
+            window.open(whatsappLink, "_blank");
+        });
+        formik.resetForm();
+    };
+    
+    const handleEmailSend = async () => {
+        const errors = await formik.validateForm();
+
+        if (Object.keys(errors).length > 0) {
+            formik.setTouched({
+                name: true,
+                email: true,
+                subject: true,
+                message: true,
+            });
+            alert("Please complete all required form fields.");
+            return;
+        }
+        
+        const formDataText = `Name: ${formik.values.name}\nEmail: ${formik.values.email}\nSubject: ${formik.values.subject}\nMessage: ${formik.values.message}`;
+        
+        // Prepare email data to send to server
+        const emailData = {
+            name:formik.values.name,
+            from: formik.values.email,
+            subject: formik.values.subject,
+            body: formDataText.replace(/\n/g, '<br>'), // Convert line breaks to HTML for the email body
+            attachments: files,
+        };
+        
+        // Send email data to server
+        try {
+            const formData = new FormData();
+            formData.append('emailData', JSON.stringify(emailData));
+            files.forEach((file) => {
+                formData.append('attachments', file);
+            });
+        
+            const response = await fetch('https://fedel-server.vercel.app/get-quote-email', {
+                method: 'POST',
+                body: formData,
+            });
+        
+            if (response.ok) {
+                alert('Email sent successfully!');
+            } else {
+                alert('Failed to send email');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred while sending the email');
+        }
+        formik.resetForm();        
+    };
+
+    return (
+        <AnimatePresence>
+            <motion.div 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed inset-0 flex items-center justify-center
+            bg-black bg-opacity-80 z-50">
+                <motion.div 
+                initial={{ y: 0, opacity: 0.7 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: 10, opacity: 0 }}
+                transition={{ duration: 0.1 }}
+                className="bg-white md:p-8 ss:p-8 p-4 padphone rounded-2xl 
+                shadow-xl flex flex-col md:justify-center ss:justify-center 
+                w-auto md:h-auto ss:h-auto h-[80%] overflow-auto 
+                items-center relative md:m-0 ss:m-16 m-6">
+                    <button
+                        className='absolute md:top-8 ss:top-8 top-4 
+                        md:right-8 ss:right-8 right-4 text-main 
+                        md:text-[20px] ss:text-[20px] text-[18px] 
+                        cursor-pointer phone2'
+                        onClick={closeQuoteModal}
+                    >
+                        <HiX />
+                    </button>
+
+                    <div className='flex flex-col w-full md:gap-8
+                    ss:gap-8 gap-5'>
+                        <div className="relative">
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder='Enter your name'
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className="md:py-2.5 ss:py-2 py-1.5 md:px-3 
+                                ss:px-3 px-2 border-search 
+                                text-black md:rounded-lg rounded-md md:text-[13px]
+                                ss:text-[14px] text-[12px]
+                                bg-transparent w-full placeholder:text-main3"
+                            />
+                            <p className="text-mainRed md:text-[12px] 
+                            ss:text-[12px] text-[11px] md:mt-2 ss:mt-2 mt-1">
+                                {formik.touched.name && formik.errors.name}
+                            </p>
+                        </div>
+
+                        <div className="relative">
+                            <input
+                                type="text"
+                                name="email"
+                                placeholder='Enter your email'
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className="md:py-2.5 ss:py-2 py-1.5 md:px-3 
+                                ss:px-3 px-2 border-search 
+                                text-black md:rounded-lg rounded-md md:text-[13px]
+                                ss:text-[14px] text-[12px]
+                                bg-transparent w-full placeholder:text-main3"
+                            />
+                            <p className="text-mainRed md:text-[12px] 
+                            ss:text-[12px] text-[11px] md:mt-2 ss:mt-2 mt-1">
+                                {formik.touched.email && formik.errors.email}
+                            </p>
+                        </div>
+
+                        <div className="relative">
+                            <input
+                                type="text"
+                                name="subject"
+                                placeholder='Enter a subject (e.g. Inquiry for Glass Marble Tile)'
+                                value={formik.values.subject}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className="md:py-2.5 ss:py-2 py-1.5 md:px-3 
+                                ss:px-3 px-2 border-search 
+                                text-black md:rounded-lg rounded-md md:text-[13px]
+                                ss:text-[14px] text-[12px]
+                                bg-transparent w-full placeholder:text-main3"
+                            />
+                            <p className="text-mainRed md:text-[12px] 
+                            ss:text-[12px] text-[11px] md:mt-2 ss:mt-2 mt-1">
+                                {formik.touched.subject && formik.errors.subject}
+                            </p>
+                        </div>
+
+                        <div className="relative">
+                            <textarea
+                                rows="5"
+                                name="message"
+                                value={formik.values.message}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                placeholder="Enter your message"
+                                className="md:py-2.5 ss:py-2 py-1.5 md:px-3 
+                                ss:px-3 px-2 border-search 
+                                text-black md:rounded-lg rounded-md md:text-[13px]
+                                ss:text-[14px] text-[12px]
+                                bg-transparent w-full placeholder:text-main3"
+                            />
+                            <p className="text-mainRed md:text-[12px] 
+                            ss:text-[12px] text-[11px] md:mt-2 ss:mt-2 mt-1">
+                                {formik.touched.message && formik.errors.message}
+                            </p>
+                        </div>
+
+                        <div className='flex flex-col gap-0.5'>
+                            <div>
+                                <label className='inline-flex gap-2 cursor-pointer'>
+                                    <GrAttachment />
+                                    <input
+                                        type="file"
+                                        multiple
+                                        accept=".jpeg,.jpg,.png"
+                                        onChange={handleFileChange}
+                                        className="hidden"
+                                        id="fileInput"
+                                    />
+                                    <span className='text-main font-medium tracking-tight md:text-[12px] ss:text-[13px] text-[12px]'>
+                                        Attach Images
+                                    </span>
+                                </label>
+
+                                <h4 className='text-mainalt md:text-[12px] ss:text-[12px] 
+                                text-[11px] tracking-tight'>
+                                    Only JPEG, JPG and PNG less than 2MB allowed
+                                </h4>
+
+                                <div className='mt-1 flex gap-3'>
+                                    {previews.map((preview, index) => (
+                                        <img
+                                            key={index}
+                                            src={preview}
+                                            alt={`Preview ${index}`}
+                                            className='w-10 h-auto object-cover rounded-md'
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="flex gap-2 w-full mt-1">
+                            <button
+                            type="button"
+                            className="bg-primary grow5 md:text-[13px] w-full
+                            ss:text-[14px] text-[11px] md:py-3 ss:py-3 py-2 
+                            text-white md:rounded-lg rounded-md border-none"
+                            onClick={handleEmailSend}
+                            >
+                               Send Email
+                            </button>
+
+                            <button
+                            type="button"
+                            className="bg-green grow5 md:text-[13px] w-full
+                            ss:text-[14px] text-[11px] md:py-3 ss:py-3 py-2 
+                            text-white md:rounded-lg rounded-md border-none"
+                            onClick={handleWhatsAppSend}
+                            >
+                                Send via WhatsApp
+                            </button>
+                        </div>
+                    </div>
+                </motion.div>
+            </motion.div>
+        </AnimatePresence>
+    );
+};
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false);
@@ -25,6 +323,7 @@ const Navbar = () => {
     const cartItems = useSelector((state) => state.cart.items);
     const itemCount = cartItems.length;
     const [scrollPosition, setScrollPosition] = useState(0);
+    const [isQuoteModalOpen, setIsQuoteModalOpen] = useState(false);
 
     const { slug, categorySlug } = useParams();
     const currentCategory = slug || categorySlug || '';
@@ -62,6 +361,13 @@ const Navbar = () => {
         } else {
             console.error('Invalid categorySlug or productSlug');
         }
+    };
+
+    const handleQuoteModalOpen = () => {
+        setIsQuoteModalOpen(true);
+        setScrollPosition(window.pageYOffset);
+        document.body.style.overflow = 'hidden';
+        document.body.style.top = `-${scrollPosition}px`;
     };
 
     useEffect(() => {
@@ -315,9 +621,7 @@ const Navbar = () => {
 
                             <button className='bg-primary text-[13px] py-3 px-5
                             text-white rounded-[10px] grow4 cursor-pointer w-[160px]'
-                            // onClick={() => {
-                            //     setToggle(!toggle);
-                            // }}
+                            onClick={handleQuoteModalOpen}
                             >
                                 Get a Quote
                             </button>
@@ -484,7 +788,11 @@ const Navbar = () => {
                             <button className='bg-primary ss:py-4 
                             py-3 px-4 w-full text-white rounded-[8px] 
                             mt-4 ss:text-[17px] text-[13px]' 
-                            onClick={() => setToggle(!toggle)}>
+                            onClick={() => {
+                                setToggle(!toggle);
+                                handleQuoteModalOpen();
+                            }}
+                            >
                                 Get a Quote
                             </button>
 
@@ -517,6 +825,12 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
+
+        {isQuoteModalOpen && (
+            <QuoteModal
+                onClose={() => setIsQuoteModalOpen(false)}
+            />
+        )}
     </nav>
   );
 };
