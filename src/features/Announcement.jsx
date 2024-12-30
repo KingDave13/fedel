39f67b067946525c488b278dac6132d894a78f2a
David Okaliwe <okaliwedavid@gmail.com>
2024-12-30 11:16:07 +0000

    Updates

--- src/features/Announcement.jsx
+++ src/features/Announcement.jsx
@@ -7,7 +7,7 @@ const Announcement = ({ bannerText }) => {
   return (
     <div className='w-full bg-primary sticky top-0 z-50 font-encode-sans'>
         {bannerTextPresent && (
-            <div className='md:py-4 ss:py-5 py-4 overflow-hidden flex gap-3'>
+            <div className='py-4 overflow-hidden flex gap-3'>
                 <div className='flex justify-between gap-3 animate-slide-left'>
                     <h2 className="text-white whitespace-nowrap 
                     md:text-[15px] ss:text-[16px] text-[14px]">
